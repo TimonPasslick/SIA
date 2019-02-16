d645858991615eea03e9f9d3702a6dc02e45d79c
Timon Paßlick <timon.passlick@gmx.de>
2019-02-16 19:06:39 +0100

    ziemlich viel auf einmal
    Eigentlich alles, was wir Freitag geändert und nicht direkt gepusht haben - kein Überblick :cry:

--- sketch_SIAC_refactored.ino
+++ sketch_SIAC_refactored.ino
@@ -1,89 +1,109 @@
-// Arduino
+//Arduino
 #include <Servo.h>
 
-// third party
-#include "DistanceGP2Y0A21YK.h"
+//third party
 #include "radio_receiver.h"
 
-// siaLib
-#include "define.h" // #defines RADIO_RECEIVER_NULL_CH_n, 0 <= n <= 9
-#include "sia_board_io.h" // #defines pin names
+//siaLib
+#include "define.h" //#defines RADIO_RECEIVER_NULL_CH_n, 0 <= n <= 9
+#include "sia_board_io.h" //#defines pin names
 
 
 Servo motor;
 Servo frontAxis, rearAxis;
 
-DistanceGP2Y0A21YK  distanceSensors[4];
-
 radio_receiver controllerSignalReceiver;
+
+/////////NEEDS SOME FINETUNING/////////
+constexpr int
+steerSignalMaxPossible = 50,
+steerSignalMaxAllowed = 20;
 
 
 constexpr int
-  steerSignalMultiplier = 2,
-  speedSignalMultiplier = 2,
-  speedSignalMax = 85,
-  motorSpeedStop = 75,
-  minDistanceCm = 15;
+speedSignalMinPossible = -50,
+speedSignalMinAllowed = -50,
+
+speedSignalMaxPossible = 50,
+speedSignalMaxAllowed = 50,
+
+stopSpeed = 75;
+///////////////////////////////////////
 
 
 void setup() {
-  motor.attach(SERVO_MC_ONE_OUT);
-  motor.write(motorSpeedStop);
-
+  motor    .attach(SERVO_MC_ONE_OUT);
   frontAxis.attach(SERVO_TWO_OUT);
-  frontAxis.write(90);
-
-  rearAxis.attach(SERVO_ONE_OUT);
-  rearAxis.write(90);
-
-  distanceSensors[0].begin(ANALOG_IN_0);
-  distanceSensors[1].begin(ANALOG_IN_1);
-  distanceSensors[2].begin(ANALOG_IN_2);
-  distanceSensors[3].begin(ANALOG_IN_3);
+  rearAxis .attach(SERVO_ONE_OUT);
 
   controllerSignalReceiver.init();
-
-  delay(1000);
 }
 
+//Running the loop only 25 times a second is responsive enough and saves battery.
+void loop25Hz();
 
 void loop() {
-  // STEER
-  {
-    const int steerSignal =  controllerSignalReceiver.AverageChannel(1) - RADIO_RECEIVER_NULL_CH_1;
+  constexpr int loopTime = 1000 / 25;
 
-    frontAxis.write(90 - steerSignalMultiplier * steerSignal);
-    rearAxis .write(90 + steerSignalMultiplier * steerSignal);
+  const auto start = millis();
+
+  loop25Hz();
+
+  const auto used = millis() - start;
+
+  if (used < loopTime)
+    delay(loopTime - used);
+}
+
+int limit(int value, const int maxPossible, const int maxAllowed) {
+  value = map(value, 0, maxPossible, 0, maxAllowed);
+
+  if (value > maxAllowed)
+    value = maxAllowed;
+
+  return value;
+}
+
+void loop25Hz() {
+  //channel number is arbitrary choice: all channels are 0 if controller is switched of
+  const bool off = controllerSignalReceiver.AverageChannel(0) < 20;
+
+  //STEER
+  {
+    int steerSignal = controllerSignalReceiver.AverageChannel(1) - RADIO_RECEIVER_NULL_CH_1;
+
+    steerSignal = limit(steerSignal, steerSignalMaxPossible, steerSignalMaxAllowed);
+
+    if (off)
+      steerSignal = 0;
+
+    frontAxis.write(90 - steerSignal);
+    rearAxis .write(90 + steerSignal);
   }
 
-  // SPEED
+  //SPEED
   {
     int speedSignal = controllerSignalReceiver.AverageChannel(2) - RADIO_RECEIVER_NULL_CH_2;
 
-    if (speedSignal > speedSignalMax)
-      speedSignal = speedSignalMax;
+    speedSignal = -speedSignal; //controller or servo is strange
 
-    int motorSpeed = (speedSignalMultiplier * speedSignal) + motorSpeedStop;
+    if (speedSignal > 0)
+      speedSignal = limit(speedSignal, speedSignalMaxPossible, speedSignalMaxAllowed);
+    if (speedSignal < 0)
+      speedSignal = -limit(-speedSignal, -speedSignalMinPossible, -speedSignalMinAllowed);
 
-    { // stop check
+    { //stop check
       static int prevSpeedSignal = 0;
 
-      const bool switchingDirection = (prevSpeedSignal >= 0 and speedSignal < 0) or
-                                      (prevSpeedSignal <= 0 and speedSignal > 0);
+      const bool switchingDirection = (prevSpeedSignal > 0 and speedSignal < 0) or
+                                      (prevSpeedSignal < 0 and speedSignal > 0);
 
-      bool sensorsTooClose = false;
-      for (auto& sensor : distanceSensors) {
-        const int distance = sensor.getDistanceCentimeter();
-        if (distance >= 0 and distance < minDistanceCm)
-          sensorsTooClose = true;
-      }
-
-      if (switchingDirection or sensorsTooClose)
-        motorSpeed = motorSpeedStop;
+      if (switchingDirection or off)
+        speedSignal = 0;
 
       prevSpeedSignal = speedSignal;
     } //stop check end
 
-    motor.write(motorSpeed);
+    motor.write(speedSignal + stopSpeed);
   }
 }
