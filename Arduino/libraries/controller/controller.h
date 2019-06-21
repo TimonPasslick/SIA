5c0ba96e440596c3fa0dab4f2149ab8f1099eee1
GaiusAnker <steffen10021211@gmail.com>
2019-06-21 14:56:23 +0200

    Update controller.h
    Überarbeitet

--- Arduino/libraries/controller/controller.h
+++ Arduino/libraries/controller/controller.h
@@ -1,12 +1,14 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
 #include <Servo.h>
 #include "sia_board_io.h"
 #include "Adafruit_NeoPixel.h"
 #include "hardware.h"
 
-namespace control {
+namespace controller {
 
 // const
-constexpr int maxSpeed = 13, stopSpeed = 90;
 constexpr int maxLenkWinkel = 20, stopLenkWinkel = 90;
 constexpr int BLINKER_LENGTH = 3;
 
@@ -15,10 +17,17 @@ unsigned long previousSpeedMillis = 0;
 unsigned long previousLenkMillis = 0;
 unsigned long previousBlinkerMillis[] = {0, 0};
 
-// Delays
+#ifdef CAR1
+constexpr int maxVorSpeed = 13, stopSpeed = 90, maxRueckSpeed = 13;
+#else
+constexpr int maxVorSpeed = 13, stopSpeed = 75, maxRueckSpeed = 13;
+#endif
+
+
 constexpr int BESCHL_DELAY = 150;
-constexpr int LENK_DELAY = 30;
+constexpr int LENK_DELAY = 15;
 constexpr int STD_BLINKER_DELAY = 125;
+
 
 // Enums
 enum {VORWAERTS = 0, RUECKWAERTS = 1};
@@ -45,6 +54,7 @@ int reachLenkWinkel = 0;
 
 //////////////////////////// HILFS-FUNKTIONEN/////////////////////////////////////
 
+// Git Objekt fpr LED-Streifen zurück (side = 0 => links |||| side = 1 => rechts)
 Adafruit_NeoPixel* getSide(int side) {
   if (side == 0) {
     return &blinkerLinks;
@@ -53,10 +63,12 @@ Adafruit_NeoPixel* getSide(int side) {
   }
 }
 
+// Wird aufgerufen zum Blinker einschalten
 void blinkerAnschalten(int side) {
   blinkerAn[side] = true;
 }
 
+// Wird aufgerufen zum Blinker ausschalten
 void blinkerAusschalten(int side) {
   blinkerAn[side] = false;
   blinkerPos[side] = 0;
@@ -65,6 +77,7 @@ void blinkerAusschalten(int side) {
   strip->show();
 }
 
+// Wenn die Lenkung halb auf eine Seite eingeschlagen hat, wird geblinkt
 void autoBlink() {
   // Wenn die Warnblinker an ist
   if (warnBlinker)
@@ -89,6 +102,7 @@ void autoBlink() {
 
 /////////////////////////////AUFGERUFENE-FUNKTIONEN//////////////////////////////////////
 
+// Beide Blinker werden eingeschaltet
 void warnblinkerAn() {
   warnBlinker = true;
   for (int i = 0; i < 2; i++) {
@@ -98,6 +112,7 @@ void warnblinkerAn() {
   }
 }
 
+// Beide Blinker werden ausgeschaltet
 void warnblinkerAus() {
   warnBlinker = false;
   for (int i = 0; i < 2; i++) {
@@ -105,7 +120,7 @@ void warnblinkerAus() {
   }
 }
 //---------Licht------------------/
-void fahrtlichtAnschalten() { // Ist immer an => keine ausschaltfunktion....?
+void fahrtlichtAnschalten() { // Ist immer an => keine ausschaltfunktion
   vorneFahrtlicht.fill(0xFF);
   vorneFahrtlicht.show();
 
@@ -115,6 +130,7 @@ void fahrtlichtAnschalten() { // Ist immer an => keine ausschaltfunktion....?
   hintenFahrtUndRueckFahrLicht.show();
 }
 
+// Rückfahrlicht wird nur beim Rückwärtsfahren angemacht (sind die beiden äußeren LEDs des hinteren LED-Streifen)
 void rueckfahrlichtAnschalten() {
   rueckfahrLichtAn = true;
   hintenFahrtUndRueckFahrLicht.setPixelColor(0, 0xDD, 0xDD, 0xDD);
@@ -122,6 +138,7 @@ void rueckfahrlichtAnschalten() {
   hintenFahrtUndRueckFahrLicht.show();
 }
 
+// Schaltet Rückfahrlicht aus
 void rueckfahrlichtAusschalten() {
   rueckfahrLichtAn = false;
   hintenFahrtUndRueckFahrLicht.setPixelColor(0, 0x00, 0x00, 0x00);
@@ -130,20 +147,22 @@ void rueckfahrlichtAusschalten() {
 }
 
 //---------Servo------------------/
-/* ---------------------
-   | min:         max: |
-   | -13 < geschw < 13 |
-   ---------------------
+/* -----------------------
+   | min:           max: |
+   | -100 < geschw < 100 |
+   -----------------------
 */
+// Muss zum fahren aufgerufen werden 100 = max | 0 = stop | -100 = maxRück
 void fahren(int geschw) {
-  if (geschw < maxSpeed * -1)
-    reachSpeed = maxSpeed * -1;
-  else if (geschw > maxSpeed)
-    reachSpeed = maxSpeed;
-  else
-    reachSpeed = geschw;
+	if (geschw < -100)
+		geschw = -100;
+	else if (geschw > 100)
+		geschw = 100;
+	
+	reachSpeed = map(geschw, -100, 100, maxRueckSpeed*-1, maxVorSpeed);
 }
 
+// Muss aufgerufen werden zum anhalten
 void anhalten() {
   reachSpeed = 0;
 }
@@ -153,6 +172,7 @@ void anhalten() {
    | -20 < winkel < 20 |
    ---------------------
 */
+// Lenken -20 = links | 0 = gerade | 20 rechts
 void lenken(int winkel) {
   if (winkel < maxLenkWinkel * -1)
     reachLenkWinkel = maxLenkWinkel * -1;
@@ -162,12 +182,14 @@ void lenken(int winkel) {
     reachLenkWinkel = winkel;
 }
 
+// Muss aufgerufen werden um Zweiachslenkung an-/auszuschakten
 void zweiachslenkung(bool on) {
-  zweiachsLenkung = on; // TODO: Wenn manes ausschaltet, sollte die hintere Achse wieder gerade sein... aber auch nicht Lnken im stand.....
+  zweiachsLenkung = on; 
 }
 
 //////////////////////////// INIT ///
 
+// setzt alles zurück (hält an, etc.)
 void reset() {
   // Blinker ausschalten
   for (int i = 0; i < 2; i++) {
@@ -185,28 +207,30 @@ void reset() {
 }
 
 ////////////////////////////LOOP-FUNKTIONEN////////////////////////////////
+// Diese Funktionen üssen bei jedem Loop Durchlauf aufgerufen werden!
 
 void loopBlinker() {
   for (int i = 0; i < 2; i++) {
-    if (!blinkerAn[i])
-      continue;
+    if (!blinkerAn[i]) // Wenn der Blinker nicht an ist...
+      continue; // Zum Anfang der Schleife
 
     Adafruit_NeoPixel *strip = getSide(i);
     unsigned long currentMillis = millis();
-    if (currentMillis - previousBlinkerMillis[i] > currentBlinkerDelay[i]) {
+    if (currentMillis - previousBlinkerMillis[i] > currentBlinkerDelay[i]) { // Blinker Animation (delay)
       previousBlinkerMillis[i] = currentMillis;
-      currentBlinkerDelay[i] = (BLINKER_LENGTH - 1 == blinkerPos[i] ? 500 : 125);
+      currentBlinkerDelay[i] = (BLINKER_LENGTH - 1 == blinkerPos[i] ? 500 : 125); // Sollen länger leuchten, wenn alle leuchten
 
       if (blinkerPos[i] >= BLINKER_LENGTH) { // alle Blinker werden ausgeschaltet
         strip->fill(0x00);
         blinkerPos[i] = 0;
       } else { // Blinker werden nacheinander eingeschaltet
-#ifdef CAR2
+// Die Blinker sind bei den Autos unterschiedlich angebracht => unterschiedliche Blinkrichtung	  
+#ifdef CAR1
         strip->setPixelColor((BLINKER_LENGTH - 1) - blinkerPos[i], 0xFF, 0x45, 0x00);
 #else
         strip->setPixelColor(blinkerPos[i], 0xFF, 0x45, 0x00);
 #endif
-        blinkerPos[i]++;
+        blinkerPos[i]++; // nächstes LED am LED-Streifen welches angehen soll
       }
     }
     strip->show();
@@ -214,38 +238,38 @@ void loopBlinker() {
 }
 
 void loopSpeed() {
-  if (reachSpeed == currentSpeed)
+  if (reachSpeed == currentSpeed) // Wenn die Geschwindigkeit welche erreicht werden soll, bereits erreicht ist:
     return;
 
   unsigned long currentMillis = millis();
-  if (currentMillis - previousSpeedMillis > BESCHL_DELAY) {
+  if (currentMillis - previousSpeedMillis > BESCHL_DELAY) { // Langsam schneller/langsamer werden
     previousSpeedMillis = currentMillis;
 
-    if (reachSpeed > currentSpeed) { // vergrößern
+    if (reachSpeed > currentSpeed) { // schneller werden
       currentSpeed++;
-    } else {
+    } else { // langsamer werden
       currentSpeed--;
     }
     motor.write(stopSpeed + currentSpeed);
 
     // Rückfahrlicht
-    if (currentSpeed < 0 && !rueckfahrLichtAn)
+    if (currentSpeed < 0 && !rueckfahrLichtAn) // Fährt Rückwärts
       rueckfahrlichtAnschalten();
-    else if (currentSpeed >= 0 && rueckfahrLichtAn)
+    else if (currentSpeed >= 0 && rueckfahrLichtAn) // Fährt vorwärts
       rueckfahrlichtAusschalten();
   }
 }
 
 void loopLenken() {
-  if (reachLenkWinkel == currentLenkWinkel)
+  if (reachLenkWinkel == currentLenkWinkel) // Wenn der Lenkwinkel, welcher erreicht werden soll bereits erreicht ist.
     return;
 
-  // KEIN lenken im Stand.
-  if (currentSpeed > (int)(maxSpeed / 3) * -1 || currentSpeed < (int)(maxSpeed / 3))
-    return;
+  // KEIN lenken im Stand. (muss noch gemacht werden)
+  /*if (currentSpeed > (int)(maxSpeed / 3) * -1 || currentSpeed < (int)(maxSpeed / 3))
+    return;*/
 
   unsigned long currentMillis = millis();
-  if (currentMillis - previousLenkMillis > LENK_DELAY) {
+  if (currentMillis - previousLenkMillis > LENK_DELAY) { // langsam lenken
     previousLenkMillis = currentMillis;
 
     if (reachLenkWinkel > currentLenkWinkel) { // vergrößern | rechts
@@ -258,11 +282,12 @@ void loopLenken() {
     autoBlink();
 
     frontAxis.write(stopLenkWinkel + currentLenkWinkel);
-    if (zweiachsLenkung) {
+    if (zweiachsLenkung) { // Hinterachse wird entgegengesetzt angesteuert
       hinterachseLenkWinkel = currentLenkWinkel * -1;
       rearAxis.write(stopLenkWinkel + hinterachseLenkWinkel);
     } else if (hinterachseLenkWinkel != 0) { // Wenn Zweiachslenkung ausgeschaltet wurde und die Hintere Achse noch schräg ist...
 
+		// Wird wieder auf mittelstellung gebracht, wenn vorne auch gelenkt wird.
       if (hinterachseLenkWinkel > 0)
         hinterachseLenkWinkel--;
       else
@@ -276,9 +301,7 @@ void loopLenken() {
 
 ///////////////////////////////HAUPT-FUNKTIONEN////////////////////////////////////////////
 
-unsigned long pm;
-int mode;
-
+// Wird aufgerufen und auf standartwerde gesetzt
 void setupController() {
   Serial.begin(9600);
 
@@ -307,88 +330,13 @@ void setupController() {
   }
   delay(100);
   // ---
-
-  //test
-  mode = 0;
-
-  fahren(0);
 }
 
+// Ruft die 3 loopFunktionen auf. (Damit man nicht jede einzeln aufrufen muss)
 void loopController() {
-
-  // Testen...
-  unsigned long m = millis();
-  /*if (m - pm > 5000) {
-    pm = m;
-
-    Serial.print("Mode: ");
-    Serial.println(mode);
-
-    if (mode == 0)
-      fahren(13);
-    else if (mode == 1)
-      lenken(-20);
-    else if (mode == 2)
-      lenken(0);
-    else if (mode == 3)
-      anhalten();
-    else if (mode == 4)
-      lenken(-20);
-    else if (mode == 5)
-      fahren(13);
-    else if (mode == 6)
-      lenken(0);
-    else if (mode == 7)
-      anhalten();
-    else if (mode == 8)
-      fahren(-13);
-    else if (mode == 9) {
-      anhalten();
-      warnblinkerAn();
-      zweiachslenkung(true);
-    }
-    else if (mode == 10) {
-      fahren(13);
-      warnblinkerAus();
-    } else if (mode == 11)
-      lenken(-20);
-    else if (mode == 12)
-      lenken(0);
-    else if (mode == 13)
-      anhalten();
-    else if (mode == 14)
-      lenken(-20);
-    else if (mode == 15)
-      fahren(13);
-    else if (mode == 16)
-      lenken(0);
-    else if (mode == 17)
-      anhalten();
-    else if (mode == 18)
-      fahren(-13);
-    else if (mode == 19)
-      anhalten();
-    else if (mode == 20)
-      zweiachslenkung(false);
-    else if (mode == 21)
-      fahren(100);
-    else if (mode == 22)
-      anhalten();
-    else if (mode == 23)
-      warnblinkerAn();
-    else if (mode == 24)
-      lenken(-20);
-    else if (mode == 25)
-      fahren(1123);
-    else if (mode == 26)
-      anhalten();
-
-
-    mode++;
-  }*/
-
   loopBlinker();
   loopSpeed();
   loopLenken();
 }
 }
+#endif
